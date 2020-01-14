cf7a73807ef5a5476a4003a281d004fed418d29d
Jyoti2005 <59308794+Jyoti2005@users.noreply.github.com>
2020-01-14 16:33:21 +0530

    Add files via upload

---
+++ src/codein/EncapsulationDemo.java
@@ -0,0 +1,31 @@
+package codein;
+public class EncapsulationDemo 
+{
+private int age;
+private String name;
+private int roll;
+public int getAge()
+{
+return age;
+}
+public String getName()
+{
+return name;
+}
+public int getRoll()
+{
+return roll;
+}
+public void setAge(int newAge)
+{
+age=newAge;
+}
+public void setName(String newName)
+{
+name=newName;
+}
+public void setRoll(int newRoll)
+{
+roll=newRoll;
+}
+}
